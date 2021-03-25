b2e2c512ad5171e6ab9cd11e251e22725d80d3b8
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-03-25 21:39:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,30 +21,30 @@ def dichotomy(a0, b0, f):
         en -= 1
     return f((a0 + b0) / 2)
 
-# def parabola_method(a0, b0, f):
-#     x1 = a0
-#     x3 = b0
-#     f1 = f(x1)
-#     f3 = f(x3)
-#
-#     un = 3
-#     while (un >= 1):
-#         x2 = (x1 + x3) / 2
-#         f2 = f(x2)
-#         u = x2 - ((x2-x1)**2 * (f2-f3) - (x2-x3)**2 * (f3-f1)) / (2 * ((x2-x1) * (f2-f3) - (x2-x3) * (f2-f1)))
-#         print(x1, x2, x3, f1, f2, f3, u, f(u))
-#         if f2 < f1 and f2 < f3:
-#             if x2 < u:
-#                 x1 = x2
-#                 x3 = u
-#                 f1 = f(x1)
-#                 f3 = f(x3)
-#             else:
-#                 x1 = u
-#                 x3 = x1
-#                 f1 = f(x3)
-#                 f3 = f(x1)
-#         un -= 1
+def parabola_method(a0, b0, f):
+    x1 = a0
+    x3 = b0
+    f1 = f(x1)
+    f3 = f(x3)
+
+    un = 3
+    while (un >= 1):
+        x2 = (x1 + x3) / 2
+        f2 = f(x2)
+        u = x2 - ((x2-x1)**2 * (f2-f3) - (x2-x3)**2 * (f3-f1)) / (2 * ((x2-x1) * (f2-f3) - (x2-x3) * (f2-f1)))
+        print(x1, x2, x3, f1, f2, f3, u, f(u))
+        if f2 < f1 and f2 < f3:
+            if x2 < u:
+                x1 = x2
+                x3 = u
+                f1 = f(x1)
+                f3 = f(x3)
+            else:
+                x1 = u
+                x3 = x1
+                f1 = f(x3)
+                f3 = f(x1)
+        un -= 1
 
 print ('dichotomy: %s' % dichotomy(a0, b0, func))
-# parabola_method(a0, b0, func)
+parabola_method(a0, b0, func)
