df1edf4132ba801816ca803ef1d725fe61ab2c75
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-03-26 00:06:11 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,35 @@
+import math
+
+func = lambda x: math.sin(x) * x ** 3
+
+a0, b0 = -7, -4
+
+e = 0.0001
+
+def parabola_method(a0, b0, f):
+    x1 = a0
+    x3 = b0
+    f1 = f(x1)
+    f3 = f(x3)
+
+    en = 0
+    while math.fabs(x3 - x1) > e:
+        x2 = (x1 + x3) / 2
+        f2 = f(x2)
+        u = x2 - ((x2 - x1) ** 2 * (f2 - f3) - (x2 - x3) ** 2 * (f3 - f1)) / (
+                    2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)))
+        # print(x1, x2, x3, f1, f2, f3, u, f(u))
+        if x2 < u:
+            x1 = x2
+            x3 = u
+            f1 = f(x1)
+            f3 = f(x3)
+        else:
+            x1 = u
+            x3 = x1
+            f1 = f(x3)
+            f3 = f(x1)
+        en += 1
+    return f((x1 + x3) / 2)
+
+print('parabola_method: %s' % parabola_method(a0, b0, func))
