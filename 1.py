549784ed1e9fe5b726d1bd464d992565e7ef8fa2
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-03-26 20:57:33 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,8 +9,9 @@ e = 0.001
 def dichotomy(a0, b0):
     en = math.log((b0 - a0) / e) / math.log(2)
     print(en)
+    en = 0
     d = 0.1 * (e / 2)
-    while en >= 1:
+    while (b0 - a0) / 2 >= e:
         x = (a0 + b0) / 2
         x1 = x - d
         x2 = x + d
@@ -18,7 +19,8 @@ def dichotomy(a0, b0):
             a0 = x1
         else:
             b0 = x2
-        en -= 1
+        en += 1
+    print(en)
     return (a0 + b0) / 2, f((a0 + b0) / 2)
 
 print('dichotomy (x;y): %s %s' % dichotomy(a0, b0))
