18ac77bad3d8b1051492c42cdaa5b14641dd9909
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-03-26 21:00:09 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -15,8 +15,7 @@ def parabola_method(a0, b0):
     while x3 - x1 > e:
         x2 = (x1 + x3) / 2
         f2 = f(x2)
-        u = x2 - ((x2 - x1) ** 2 * (f2 - f3) - (x2 - x3) ** 2 * (f3 - f1)) / (
-                    2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)))
+        u = x2 - ((x2-x1)**2 * (f2-f3) - (x2-x3)**2 * (f3-f1)) / (2 * ((x2-x1) * (f2-f3) - (x2-x3) * (f2-f1)))
         fu = f(u)
         if u < x2:
             left = u
