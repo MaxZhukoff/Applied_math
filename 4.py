5e8f062a87a0c8b1c85fc8e32d40f9ec8616024d
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-03-26 22:10:10 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -18,21 +18,15 @@ def parabola_method(a0, b0):
         u = x2 - ((x2-x1)**2 * (f2-f3) - (x2-x3)**2 * (f3-f1)) / (2 * ((x2-x1) * (f2-f3) - (x2-x3) * (f2-f1)))
         fu = f(u)
         if u < x2:
-            left = u
-            fl = fu
-            right = x2
-            fr = f2
+            left, fl = u, fu
+            right, fr = x2, f2
         else:
-            left = x2
-            fl = f2
-            right = u
-            fr = fu
+            left, fl = x2, f2
+            right, fr = u, fu
         if fl < fr:
-            x3 = right
-            f3 = fr
+            x3, f3 = right, fr
         elif fl > fr:
-            x1 = left
-            f1 = fl
+            x1, f1 = left, fl
         en += 1
     print(en)
     return (x1 + x3) / 2, f((x1 + x3) / 2)
