a00a7a2f848bad1d457432591dde181143c93cbb
pavelspace <pavelkvetkin@gmail.com>
2021-03-26 18:11:20 +0600

    Create main.py

---
+++ 01/main.py
@@ -0,0 +1,39 @@
+import math
+
+func = lambda x: math.sin(x) * x ** 3
+
+epsilon = 0.01
+
+
+def dichotomy_method(a0, b0):
+    iterations = math.log((b0 - a0) / epsilon) / math.log(2)
+    delta = epsilon / 2 - 0.001
+    while iterations >= 1:
+        x = (a0 + b0) / 2
+        x1 = x - delta
+        x2 = x + delta
+        if func(x1) > func(x2):
+            a0 = x1
+        else:
+            b0 = x2
+        iterations -= 1
+    return a0, func(a0)
+
+
+def golden_ratio_method(a0, b0):
+    x1 = a0 + ((3 - math.sqrt(5)) / 2) * (b0 - a0)
+    x2 = b0 - ((3 - math.sqrt(5)) / 2) * (b0 - a0)
+    while ((b0 - a0) / 2) >= epsilon:
+        if func(x1) > func(x2):
+            a0 = x1
+            x1 = x2
+            x2 = b0 - (x1 - a0)
+        elif func(x1) < func(x2):
+            b0 = x2
+            x2 = x1
+            x1 = a0 + (b0 - x2)
+    return (a0 + b0) / 2, func((a0 + b0) / 2)
+
+
+print(dichotomy_method(-7, -3))
+print(golden_ratio_method(-7, -3))
