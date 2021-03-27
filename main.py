8bc8e37579eecb38271c10174e9af4459901cb8d
pavelspace <pavelkvetkin@gmail.com>
2021-03-27 17:51:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ def fibonacci_method(a, b):
     fibonacci_def_plus_n = fibonacci_default(iterations + 1)
     fibonacci_def_plus_2n = fibonacci_default(iterations + 2)
     while (b - a) / 2 >= epsilon:
-        iterations -= 1
         x1 = a + (fibonacci_def_n / fibonacci_def_plus_2n) * (b - a)
         x2 = a + (fibonacci_def_plus_n / fibonacci_def_plus_2n) * (b - a)
         if f(x1) > f(x2):
