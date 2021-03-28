a399805ef1f9771f9968872db7e632185ff87103
pavelspace <pavelkvetkin@gmail.com>
2021-03-28 23:19:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ f = lambda x: math.sin(x) * x ** 3
 
 a, b = -8, -3
 
-epsilon = 0.001
+epsilon = 0.0001
 
 
 def dichotomy(a, b):
     iterations = math.log((b - a) / epsilon) / math.log(2)
+    print(iterations)
+    iterations = 0
     delta = 0.1 * (epsilon / 2)
-    en = 0
     while (b - a) / 2 >= epsilon:
         x = (a + b) / 2
         x1 = x - delta
         x2 = x + delta
-        en += 2
         if f(x1) > f(x2):
             a = x1
-        else:
+        elif f(x1) < f(x2):
             b = x2
-    print(en)
+        iterations += 1
+    print(iterations)
     return (a + b) / 2, f((a + b) / 2)
 
 
@@ -29,7 +30,7 @@ def golden_ratio_method(a, b):
     x2 = b - ((3 - math.sqrt(5)) / 2) * (b - a)
     fx1 = f(x1)
     fx2 = f(x2)
-    en = 2
+    iterations = 0
     while (b - a) / 2 >= epsilon:
         if fx1 > fx2:
             a = x1
@@ -37,21 +38,20 @@ def golden_ratio_method(a, b):
             x2 = a + ((math.sqrt(5) - 1) / 2) * (b - a)
             fx1 = fx2
             fx2 = f(x2)
-            en += 1
         elif fx1 < fx2:
             b = x2
             x2 = x1
             x1 = a + (1 - ((math.sqrt(5) - 1) / 2)) * (b - a)
             fx2 = fx1
             fx1 = f(x1)
-            en += 1
-        # print(en, a, b, math.fabs(b - a), (a + b) / 2, f((a + b) / 2))
-    print(en)
+        iterations += 1
+    print(iterations)
     return (a + b) / 2, f((a + b) / 2)
 
 
 def fibonacci_default(number):
     return (1 / math.sqrt(5)) * (((1 + math.sqrt(5)) / 2) ** number - ((1 - math.sqrt(5)) / 2) ** number)
+
 
 def fibonacci_method(a, b):
     iterations = 0
@@ -64,41 +64,41 @@ def fibonacci_method(a, b):
     x2 = a + (fibonacci_def_plus_n / fibonacci_def_plus_2n) * (b - a)
     fx1 = f(x1)
     fx2 = f(x2)
-    en = 2
+    print(b - a)
+    i = 0
     while (b - a) / 2 >= epsilon:
-        x1 = a + (fibonacci_def_n / fibonacci_def_plus_2n) * (b - a)
-        x2 = a + (fibonacci_def_plus_n / fibonacci_def_plus_2n) * (b - a)
+        i += 1
         if fx1 > fx2:
             a = x1
             x1 = x2
-            x2 = a + (fibonacci_def_plus_n / fibonacci_def_plus_2n) * (b - a)
+            x2 = b - (x1  - a)
+            # x2 = a + (fibonacci_def_plus_n / fibonacci_def_plus_2n) * (b - a)
             fx1 = fx2
             fx2 = f(x2)
-            en += 1
         elif fx1 < fx2:
             b = x2
             x2 = x1
-            x1 = a + (fibonacci_def_n / fibonacci_def_plus_2n) * (b - a)
+            x1 = a + (b - x2)
+            # x1 = a + (fibonacci_def_n / fibonacci_def_plus_2n) * (b - a)
             fx2 = fx1
             fx1 = f(x1)
-            en += 1
-    print(en)
+        print(b - a)
+    print(i)
     return (a + b) / 2, f((a + b) / 2)
+
 
 def parabola_method(a, b):
     x1 = a
     x3 = b
     f1 = f(x1)
     f3 = f(x3)
-    en = 2
+    iterations = 0
     while x3 - x1 > epsilon:
         x2 = (x1 + x3) / 2
         f2 = f(x2)
-        en += 1
         u = x2 - ((x2 - x1) ** 2 * (f2 - f3) - (x2 - x3) ** 2 * (f3 - f1)) / (
                 2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)))
         fu = f(u)
-        en += 1
         if u < x2:
             left, fl = u, fu
             right, fr = x2, f2
@@ -109,27 +109,27 @@ def parabola_method(a, b):
             x3, f3 = right, fr
         elif fl > fr:
             x1, f1 = left, fl
-    print(en)
+        iterations += 1
     return (x1 + x3) / 2, f((x1 + x3) / 2)
 
 
 def combined_brent_method(a, c):
+    iterations = 0
     k = (math.sqrt(5) - 1) / 2
     u = 0
     x = w = v = (a + c) / 2
     fx = fw = fv = f(x)
-    en = 1
     d = e = (c - a)
     while math.fabs(c - a) >= epsilon:
+        iterations += 1
         g = e
         e = d
         if x != w and x != v and v != w and fx != fw and fx != fv and fw != fv:
-            x1 = x
-            f1 = fx
-            x2 = w
-            f2 = fw
-            x3 = v
-            f3 = fv
+            arr = sorted([x, v, w])
+            x1 = arr[0]
+            x2 = arr[1]
+            x3 = arr[2]
+            f1, f2, f3 = f(x1), f(x2), f(x3)
             u = x2 - ((x2 - x1) ** 2 * (f2 - f3) - (x2 - x3) ** 2 * (f3 - f1)) \
                 / (2 * ((x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1)))
         if u >= a + epsilon and u <= c - epsilon and math.fabs(u - x) < g / 2:
@@ -142,7 +142,6 @@ def combined_brent_method(a, c):
                 u = x - k * (x - a)
                 d = x - a
         fu = f(u)
-        en += 1
         if fu <= fx:
             if u >= x:
                 a = x
@@ -167,7 +166,6 @@ def combined_brent_method(a, c):
             elif fu <= fv or x == v or w == v:
                 v = u
                 fv = fu
-    print(en)
     return x, f(x)
 
 
