823f373dddcb2d2e1f755d57b9cab4b2f03cf37a
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-05-04 20:27:39 +0300

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,17 +1,20 @@
 import math
 import numpy as np
 from one_dimensional_methods import *
-from sympy import diff, symbols, false
+from sympy import diff, symbols, false, solve
 import random
 
 variables = symbols('x y')
+
 f = lambda variables: 3 / 2 * (variables[0] - variables[1]) ** 2 + 1 / 3 * (variables[0] + variables[1]) ** 2 + 1
-epsilon = 0.01
+# 3/2*(x-y)^2 + 1/3*(x+y)^2+1
+epsilon = 0.001
+
 
 def init():
     x0 = np.array([], dtype=int)
     for i in range(0, len(variables)):
-        x0 = np.append(x0, [random.randint(-30, 30)])
+        x0 = np.append(x0, [random.randint(-10, 10)])
     return x0
 
 
@@ -42,7 +45,7 @@ def length_grad(grad):
     return length
 
 
-def gradientDescent(curr_x):
+def gradient_descent(curr_x):
     alpha = 0.1
     it = 0
     while True:
@@ -81,7 +84,7 @@ def steepest_descent(point):
     it = 0
     while True:
         it += 1
-        alpha = golden_ratio_method(lambda alp: f(next_step(curr_x, alp)), -1, 1, 0.001)
+        alpha = golden_ratio_method(lambda alp: f(next_step(curr_x, alp)), -1, 1, 0.1)
         next_x = next_step(curr_x, alpha)
         if (math.sqrt(np.absolute(length_grad(next_x) - length_grad(curr_x)))) <= epsilon:
             break
@@ -89,9 +92,68 @@ def steepest_descent(point):
     return f(next_x), it
 
 
-point1 = np.array([2, 3])
+def conjugate_gradient(point):
+    it = 0
+    k = 0
+    curr_x = point
+    p = -1 * gradient(curr_x)
+    while True:
+        it += 1
+        if k == 0:
+            p = -1 * gradient(curr_x)
+        alpha = golden_ratio_method(lambda alp: f(next_step(curr_x, alp)), -1, 1, 1)
+        next_x = curr_x + alpha * p
+        if math.sqrt(np.absolute(length_grad(gradient(next_x)))) < epsilon:
+            return f(next_x), it
+        elif k + 1 == len(point):
+            k = 0
+            curr_x = next_x
+            continue
+        else:
+            beta = (math.sqrt(np.absolute(length_grad(gradient(next_x)))) ** 2) / (
+                    math.sqrt(np.absolute(length_grad(gradient(curr_x)))) ** 2)
+            p = -1 * gradient(next_x) + beta * p
+        k += 1
+
+
+def grad_to_matrix(grad):
+    matrix_grad = np.zeros((len(grad), 1))
+    for i in range(len(grad)):
+        matrix_grad[i][0] = grad[i]
+    return matrix_grad
+
+
+def conjugate_directions(point):
+    p1 = np.array([1, 0])
+    p2 = np.array([0, 1])
+    x0 = point
+    h = symbols('h')
+    it = 0
+    while True:
+        it += 1
+        x1 = x0 + h * p2
+        hr = solve(diff(f(x1), h), h)[0]
+        x1 = x0 + hr * p2
+
+        x2 = x1 + h * p1
+        hr = solve(diff(f(x2), h), h)[0]
+        x2 = x1 + hr * p1
+
+        x3 = x2 + h * p2
+        hr = solve(diff(f(x3), h), h)[0]
+        x3 = x2 + hr * p2
+
+        p2 = x3 - x1
+        x0 = x3
+        if math.sqrt(np.absolute(length_grad(gradient(x3)))) < epsilon:
+            return f(x3), it
+
+
 x0 = init()
+# x0 = np.array([-1, 1])
 print(x0)
-print("Gradient descent:", gradientDescent(x0))
+print("Gradient descent:", gradient_descent(x0))
 print("Steepest descent:", steepest_descent(x0))
-print(step_split_gradient_descent(x0))
+print("Gradient descent second: ", step_split_gradient_descent(x0))
+print("Conjugate gradient method: ", conjugate_gradient(x0))
+print("Conjugate directions method: ", conjugate_directions(x0))
