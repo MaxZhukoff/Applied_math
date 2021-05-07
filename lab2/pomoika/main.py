26bd2f38f85a6db26d5f2b665a40bfb2c695c1c5
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-05-08 02:01:48 +0300

    Update main.py

--- lab2/pomoika/main.py
+++ lab2/pomoika/main.py
@@ -1,21 +1,23 @@
-import math
 import numpy as np
 from one_dimensional_methods import *
-from sympy import diff, symbols, false
+from four import *
+from sympy import diff, symbols, false, nsolve, sin, cos
+import random
 
-x, y = symbols('x y')
-variables = (x, y)
-f = lambda variables: 3 / 2 * (x - y) ** 2 + 1 / 3 * (x + y) ** 2 + 1
+variables = symbols('x y')
+# (1-variables[0])**2+100*(variables[1]-variables[0]**2)**2
+f = lambda variables: variables[0] ** 2 / 5 - sin(variables[0]) + variables[1] ** 2 / 5 + sin(variables[1])
+# variables[0] ** 2 / 5 + variables[1] ** 2 / 5 - sin(variables[0]) + sin(variables[1])
+# 3 / 2 * (variables[0] - variables[1]) ** 2 + 1 / 3 * (variables[0] + variables[1]) ** 2 + 1
+# 3/2*(x-y)^2 + 1/3*(x+y)^2+1
+epsilon = 0.01
 
 
-def value(values):
-    if len(values) != len(variables) or len(values) < 1:
-        return false
-    funcValue = f(values)
-    replacements = []
-    for i in range(len(values)):
-        replacements += [(variables[i], values[i])]
-    return funcValue.subs(replacements)
+def init():
+    x0 = np.array([], dtype=int)
+    for i in range(0, len(variables)):
+        x0 = np.append(x0, [random.randint(-10, 10)])
+    return x0
 
 
 def differentiation(var, point):
@@ -25,7 +27,7 @@ def differentiation(var, point):
     replacements = []
     for i in range(len(point)):
         replacements += [(variables[i], point[i])]
-    return dv.subs(replacements)
+    return dv.subs(replacements).evalf()
 
 
 def gradient(point):
@@ -40,49 +42,187 @@ def gradient(point):
 
 def length_grad(grad):
     length = 0
-    for i in range(len(x)):
-        length += grad[i]**2
+    for i in range(len(x0)):
+        length += grad[i] ** 2
     return length
 
 
-def gradientDescent(xPrev):
-    xNew = np.array([])
-    epsilon = 0.01
+def gradient_descent(curr_x):
     alpha = 0.1
     it = 0
     while True:
         it += 1
-        xNew = xPrev - alpha * gradient(xPrev)
-        # alpha /= 1.5
-        # if math.fabs(value(xNew) - value(xPrev)) <= e or all(np.absolute(xNew - xPrev) <= e):
-        if math.fabs(value(xNew) - value(xPrev)) <= epsilon:
+        next_x = curr_x - alpha * gradient(curr_x)
+        # if math.fabs(value(next_x) - value(curr_x)) <= e or all(np.absolute(next_x - curr_x) <= e):
+        if math.fabs(f(next_x) - f(curr_x)) <= epsilon:
             break
-        xPrev = xNew
-    print(value(xNew), it)
+        curr_x = next_x
+    return f(next_x), it
+
+
+def step_split_gradient_descent(curr_x):
+    alpha = 1
+    delta = 0.5
+    it = 0
+    while True:
+        it += 1
+        next_x = curr_x - alpha * gradient(curr_x)
+        if f(curr_x - alpha * gradient(curr_x)) <= f(curr_x) - epsilon * alpha * math.fabs(length_grad(curr_x)):
+            if (math.sqrt(np.absolute(length_grad(next_x) - length_grad(curr_x)))) <= epsilon:
+                break
+            curr_x = next_x
+        else:
+            if f(curr_x - alpha * gradient(curr_x)) <= f(curr_x):
+                break
+            while f(curr_x - alpha * gradient(curr_x)) > f(curr_x):
+                alpha *= delta
+    return f(next_x), it
 
 
 def next_step(curr_x, alpha):
-    return (curr_x - alpha * gradient(curr_x))
+    return curr_x - alpha * gradient(curr_x)
 
-def steepest_descent(point, epsilon):
+
+def steepest_descent(point):
     curr_x = point
     it = 0
     while True:
         it += 1
-        alpha = golden_ratio_method(lambda alp: f(next_step(curr_x, alp)), -1, 1, 0.001)
+        alpha = golden_ratio_method(lambda alp: f(next_step(curr_x, alp)), -1, 1, 0.1)
         next_x = next_step(curr_x, alpha)
         if (math.sqrt(np.absolute(length_grad(next_x) - length_grad(curr_x)))) <= epsilon:
-            print(next_x, it)
             break
-        print(next_x)
         curr_x = next_x
-    return next_x, f(next_x)
+    return f(next_x), it
 
-# point1 = np.array([2, 3])
-# print(gradient(point1))
-# val = np.array([2, 3])
-# print(value(val))
 
-x0 = np.array([3, 1])
-# gradientDescent(x0)
-steepest_descent(x0, 0.001)
+def conjugate_gradient(point):
+    it = 0
+    k = 0
+    curr_x = point
+    p = -1 * gradient(curr_x)
+    while True:
+        it += 1
+        if k == 0:
+            p = -1 * gradient(curr_x)
+        alpha = golden_ratio_method(lambda alp: f(next_step(curr_x, alp)), -1, 1, 0.1)
+        next_x = curr_x + alpha * p
+        if math.sqrt(np.absolute(length_grad(gradient(next_x)))) < epsilon:
+            return f(next_x), it
+        elif k + 1 == len(point):
+            k = 0
+            curr_x = next_x
+            continue
+        else:
+            beta = (math.sqrt(np.absolute(length_grad(gradient(next_x)))) ** 2) / (
+                    math.sqrt(np.absolute(length_grad(gradient(curr_x)))) ** 2)
+            p = -1 * gradient(next_x) + beta * p
+        k += 1
+
+
+def grad_to_matrix(grad):
+    matrix_grad = np.zeros((len(grad), 1))
+    for i in range(len(grad)):
+        matrix_grad[i][0] = grad[i]
+    return matrix_grad
+
+
+def conjugate_directions(point):  # Powell's method
+    p1 = np.array([1, 0])
+    p2 = np.array([0, 1])
+    x0 = point
+    h = symbols('h')
+    it = 0
+    while True:
+        it += 1
+        x1 = x0 + h * p2
+        hr = nsolve(diff(f(x1), h), 0)
+        x1 = x0 + hr * p2
+        x2 = x1 + h * p1
+        hr = nsolve(diff(f(x2), h), 0)
+        x2 = x1 + hr * p1
+        x3 = x2 + h * p2
+        hr = nsolve(diff(f(x3), h), 0)
+        x3 = x2 + hr * p2
+        p2 = x3 - x1
+        x0 = x3
+        if math.sqrt(np.absolute(length_grad(gradient(x3)))) < epsilon:
+            return f(x3), it
+
+
+def hessian(point):
+    size = (len(variables))
+    matrix = np.eye(size)
+    for i in range(size):
+        for j in range(size):
+            dx1 = lambda variables: diff(f(variables), variables[i])
+            dx2 = diff(dx1(variables), variables[j])
+            dx2 = dx2.subs([(variables[0], point[0]), (variables[1], point[1])]).evalf()
+            matrix[i][j] = dx2
+    return matrix
+
+
+def newton(point):
+    curr_x = point
+    it = 0
+    while True:
+        it += 1
+        next_x = curr_x.reshape(-1, 1) - np.dot(np.linalg.inv(hessian(curr_x)), gradient(curr_x).reshape(-1, 1))
+        vec_length = 0
+        for i in range(len(next_x)):
+            vec_length += next_x[i] ** 2
+        if math.sqrt(vec_length) < epsilon:
+            break
+        temp = next_x
+        next_x = np.array([])
+        for i in range(len(temp)):
+            next_x = np.append(next_x, temp[i])
+        if math.fabs(f(next_x) - f(curr_x)) <= epsilon:
+            break
+        curr_x = next_x
+    return f(next_x), it
+
+
+# x0 = init()
+x0 = np.array([-1, 1])
+print(x0)
+print("Gradient descent:", gradient_descent(x0))
+print("Steepest descent:", steepest_descent(x0))
+print("Gradient descent second: ", step_split_gradient_descent(x0))
+print("Conjugate gradient method: ", conjugate_gradient(x0))
+print("Conjugate directions method: ", conjugate_directions(x0))
+print("Newton method: ", newton(x0))
+
+# n = 2
+# cond_P = 70
+# log_cond_P = np.log(cond_P)
+# exp_vec = np.arange(-log_cond_P/4., log_cond_P * (n + 1)/(4 * (n - 1)), log_cond_P/(2.*(n-1)))
+# s = np.exp(exp_vec)
+# S = np.diag(s)
+# U, _ = np.linalg.qr((np.random.rand(n, n) - 5.) * 200)
+# V, _ = np.linalg.qr((np.random.rand(n, n) - 5.) * 200)
+# P = U.dot(S).dot(V.T)
+# matrix = P.dot(P.T)
+#
+# k = np.linalg.cond(matrix)
+# print(k)
+#
+# x_arr = symbols('x1:25')
+# x_arr = x_arr[0:n:1]
+#
+# func = 0
+# for i in range(n):
+#     func = func + x_arr[i] ** 2 * matrix.item(i, i)
+# for i in range(n):
+#     for j in range(i, n):
+#         if i == j:
+#             continue
+#         func = func + 2 * x_arr[i] * x_arr[j] * matrix.item(i, j)
+# g = lambda x_arr: func
+#
+# point = np.array([])
+# for i in range(n):
+#     point = np.append(point, int(1))
+#
+# print(func)
+# print(gradient_descent4(g, x_arr, point, epsilon))
