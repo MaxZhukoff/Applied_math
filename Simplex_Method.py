e2c020fd109fea81da6df2e275c5419052d5c091
Segway-svg <62465966+Segway-svg@users.noreply.github.com>
2021-10-27 18:22:51 +0300

    Update Simplex_Method.py

--- Simplex_Method.py
+++ Simplex_Method.py
@@ -1,4 +1,6 @@
 # SIMPLEX_METHOD
+import numpy as np
+
 
 def simplex_method(maximize_func, limitation_left_part, limitation_right_part,
                    num_of_x, limitation_symbol):
@@ -31,8 +33,17 @@ def simplex_method(maximize_func, limitation_left_part, limitation_right_part,
 
 
     # Поменять значения числе после "=" с "-" на "+", если такие имеются
+    # for i in range(len(limitation_right_part)):
+    #     if limitation_right_part[i] < 0:
+    #         for j in range(len(limitation_left_part[i])):
+    #             limitation_left_part[i][j] = -limitation_left_part[i][j]
+    #         limitation_right_part[i] = -limitation_right_part[i]
+    #         limitation_symbol[i] = -limitation_symbol[i]
+    # print()
+
+    # Поменять значения ">=" на "<="
     for i in range(len(limitation_right_part)):
-        if limitation_right_part[i] < 0:
+        if limitation_symbol[i] == 1:
             for j in range(len(limitation_left_part[i])):
                 limitation_left_part[i][j] = -limitation_left_part[i][j]
             limitation_right_part[i] = -limitation_right_part[i]
@@ -61,13 +72,13 @@ def simplex_method(maximize_func, limitation_left_part, limitation_right_part,
             if j != len(limitation_left_part[i]) - 1:
                 print(" + ", end='')
         if limitation_symbol[i] == -1:
-            limitation_left_part[i].append(0)
+            limitation_left_part[i].append(1)
             additional_x += 1
             print(f" + x{additional_x}", end='')
             mark_of_x[i] = "+"
             num_of_x[i].append(additional_x)
         if limitation_symbol[i] == 1:
-            limitation_left_part[i].append(0)
+            limitation_left_part[i].append(1)
             additional_x += 1
             print(f" - x{additional_x}", end='')
             mark_of_x[i] = "-"
@@ -95,9 +106,35 @@ def simplex_method(maximize_func, limitation_left_part, limitation_right_part,
                 basis_inequality[i][k] = -basis_inequality[i][k]
     print("\nbasis_inequality:", basis_inequality, "\nNEXT\n")
 
-    # базисы    свободные коэффициенты   x1   x2
-    # x3                  8              -4   -1
-    # x4                  3              +1   -1
+    # Симплекс таблица (отдельная функция)
+    print("Firts-Simplex-Table:")
+    # print("  C", end='      ')
+    print("   ", end='')
+    for i in range(additional_x + 1):
+        if (i < len(maximize_func)):
+            print(maximize_func[i], end='  ')
+        else:
+            print(0, end='  ')
+    print("  C")
+    print("   ", end='')
+    for i in range(1, additional_x + 2):
+        if i < additional_x + 1:
+            print(f"x{i}", end=' ')
+        else:
+            print("b", end=' ')
+    print("  basis")
+
+    simplex_table = np.zeros((len(limitation_left_part), additional_x + 1), dtype=int)
+
+    for i in range(len(num_of_x)):
+        for j in range(len(num_of_x[i])):
+            simplex_table[i][num_of_x[i][j] - 1] = limitation_left_part[i][j]
+        simplex_table[i][additional_x] = limitation_right_part[i]
+
+    print(simplex_table)
+
+    print()
+
 
 
 # F = 3x1 + 4x2 -> max
@@ -122,7 +159,6 @@ limitation_symbol = [-1, 1]
 
 simplex_method(maximize_func, limitation_left_part, limitation_right_part,
                num_of_x, limitation_symbol)
-
 
 
 
@@ -153,9 +189,3 @@ num_of_x = [[1, 2],
 limitation_symbol = [1, 1, -1, -1]
 simplex_method(maximize_func, limitation_left_part, limitation_right_part,
                num_of_x, limitation_symbol)
-
-# базисы свободные коэффициенты    x1     x2
-# x3              -2               -1     2  
-# x4              -4                1     1 
-# x5               2               -1     1
-# x6               6              empty  -1 
